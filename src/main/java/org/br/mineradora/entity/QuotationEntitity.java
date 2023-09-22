8a9de7b5e578dd853a3e7f8b246eed3605af7be2
Marcelo <marceloagsantosadv@gmail.com>
2023-09-22 09:28:46 -0300

    projetov1

---
+++ src/main/java/org/br/mineradora/entity/QuotationEntitity.java
@@ -0,0 +1,12 @@
+package org.br.mineradora.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name="quotation")
+@Data
+public class QuotationEntitity {
+
+}
